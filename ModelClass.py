e9a95d0420dc3db0f0a94c77a7fa1edc47f5081e
George Tait <george.tait512@gmail.com>
2023-11-03 18:17:53 +0000

    Added Non-Dimensionalised Model
    Created class to simulate a factory temperature with non-dimensionalised heat equation
    
    TODO:
    
    add functionality to handle arbitrary times at which heater is turned on/off

--- ModelClass.py
+++ ModelClass.py
@@ -27,7 +27,7 @@ class Factory:
         self.outdoors = outsideEnv
 
 
-    def nonDimensionalisedTemperatureDerivative(self, currentTime: float) -> float: 
+    def getTemperatureTimeDerivative(self, currentTime: float) -> float: 
 
         P_in = self.heater.heatOutputRate if self.heater.switchedOn else 0
         heatCapacity = AIR_SPECIFIC_HEAT_CAPACITY*self.surface_walls.getVolume()
@@ -40,7 +40,7 @@ class Factory:
 
 
     def updateTemperature(self, currentTime: float, dt: float):
-        dT = trapezoidal(f=self.nonDimensionalisedTemperatureDerivative, x=currentTime, dx=dt)
+        dT = trapezoidal(f=self.getTemperatureTimeDerivative, x=currentTime, dx=dt)
         self.ind_temp += dT
 
     def simulateTemperature(self, timeSamples: list[float])-> list[float]: 
@@ -56,25 +56,61 @@ class Factory:
         return temperatures
 
 
+class NDFactory: 
 
+    '''Computes non-dimensionalised heat equation for the above factory model.'''
+
+    def __init__(self, ndTemp: float, mu: float,
+                epsilon: float, env_mean_temp: float,
+                env_temp_half_width: float, heated: bool) -> None:
+        self.T = ndTemp
+        self.mu = mu
+        self.epsilon = epsilon
+        self.T_0 = env_mean_temp
+        self.T_1 = env_temp_half_width
+        self.heated = heated
+
+    def getNDTempTimeDerivative(self, currentTime: float) -> float: 
+
+        mu = self.mu if self.heated else 0
+        return mu - self.epsilon*(self.T - 1 - self.T_1*math.sin(2*math.pi*currentTime)/self.T_0)
+
+    def updateNDTemp(self, currentTime: float, dt: float) -> float: 
+        dT = trapezoidal(self.getNDTempTimeDerivative, currentTime, dt)
+        self.T += dT
+
+
+    def simulateNDTemp(self, timeSamples: list[float]) -> list[float]:
+        temperatures = []
+
+        for i in range(len(timeSamples)-1): 
+            currentTime = timeSamples[i]
+            dt = timeSamples[i+1]-timeSamples[i]
+            temperatures.append(self.T)
+            self.updateNDTemp(currentTime, dt)
+
+        return temperatures
 
 def main():
     print("Hello World \n")
     start = 0
-    end = 24
+    end = 1
     step = 0.001
-    time_points = [start + i * step for i in range(int((end - start) / step) + 1)] #measure temp over 24hrs
-    outsideEnv = OutsideEnvironment(281,4)
+    time_points = np.arange(start=start, stop=end + step, step=step)
+    # outsideEnv = OutsideEnvironment(281,4)
     
-    heatedFactory = Factory(283.0, Cuboid(50,100,20), ThermalConductivity.CONCRETE, Heater(3e5, switchedOn=True), outsideEnv, 0.05)
-    nonHeatedFactory = Factory(283.0, Cuboid(50,100,20), ThermalConductivity.CONCRETE, Heater(3e5), outsideEnv, 0.05)
-    heatedTemps = heatedFactory.simulateTemperature(time_points)
-    nonHeatedTemps = nonHeatedFactory.simulateTemperature(time_points)
+    # heatedFactory = Factory(283.0, Cuboid(50,100,20), ThermalConductivity.CONCRETE, Heater(3e5, switchedOn=True), outsideEnv, 0.05)
+    # nonHeatedFactory = Factory(283.0, Cuboid(50,100,20), ThermalConductivity.CONCRETE, Heater(3e5), outsideEnv, 0.05)
 
-    outsideTemps = [outsideEnv.getCurrentOutsideTemperature(t) for t in time_points[0:-1]]
+    heatedFactory = NDFactory(ndTemp=1, mu=1, epsilon=100, env_mean_temp=1, env_temp_half_width=0.05, heated=True)
+    nonHeatedFactory = NDFactory(1, 1, 100, 1, 0.05, False)
+    heatedTemps = heatedFactory.simulateNDTemp(time_points)
+    nonHeatedTemps = nonHeatedFactory.simulateNDTemp(time_points)
+
+    # outsideTemps = [outsideEnv.getCurrentOutsideTemperature(t) for t in time_points[0:-1]]
     plt.plot(time_points[0:-1], heatedTemps, label='Indoor Temperature w heating')
     plt.plot(time_points[0:-1], nonHeatedTemps, label="Indoor Temperature w/o heating")
-    plt.plot(time_points[0:-1], outsideTemps, label='Outdoor Temperature')
+    # plt.plot(time_points[0:-1], outsideTemps, label='Outdoor Temperature')
     plt.legend()
     plt.show()
    
